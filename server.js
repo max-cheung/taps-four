f8434561c603b77aa4fd62b390fb58225dd241b1
Max Cheung <maxcheung07@gmail.com>
2022-07-09 21:22:22 -0700

    connected server.js to mysql db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
@@ -11,3 +12,22 @@ app.listen(process.env.PORT || PORT, function() {
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'max',
+    password: 'max321',
+    database: 'taps',
+    port: '/tmp/mysql.sock'
+});
+
+db.connect;
+
+app.get('/taps', (req, res) => {
+    const sql = 'SELECT * FROM scaled_score_ages_9to95';
+
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    });
+});
